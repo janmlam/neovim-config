3723e0b965180140a0abb63fdee6ad4e926ba222
Jan <lamjanming@outlook.com>
2025-11-09 21:07:08 +0100

    Added my own mappings

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,25 +1,25 @@
 return {
-  'folke/snacks.nvim',
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    bigfile = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    notifier = {
-      enabled = true,
-      timeout = 3000,
-    },
-    terminal = {},
-  },
-  keys = {
-    {
-      '<c-/>',
-      function()
-        Snacks.terminal()
-      end,
-      desc = 'Toggle Terminal',
-    },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		bigfile = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = {
+			enabled = true,
+			timeout = 3000,
+		},
+		terminal = {},
+	},
+	keys = {
+		{
+			"<c-/>",
+			function()
+				Snacks.terminal()
+			end,
+			desc = "Toggle Terminal",
+		},
+	},
 }
